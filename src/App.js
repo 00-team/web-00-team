a84ed45c83e6e14b371bc365b15ac39814870a47
i007c <dr007cc@gmail.com>
2021-08-06 05:20:35 +0430

    install firebase module

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom'
 // style
 import './app.scss'
 
+import app from 'firebase/app';
 
 const App = () => {
+    console.log(app.SDK_VERSION);
     return (
         <div className='main'>
             <span>00 Team</span>
