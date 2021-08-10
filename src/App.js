a2dfea9e383792d479577de33a9b3fd3e9c543c7
i007c <dr007cc@gmail.com>
2021-08-10 14:22:00 +0430

    use new webpack config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// firebase
+import app from 'firebase/app';
+
 // style
 import './app.scss'
 
-import app from 'firebase/app';
+// 00 team logo
+import Logo from './img/00.png'
 
 const App = () => {
+    document.getElementById('xxx').setAttribute("content", Logo);
+
     console.log(app.SDK_VERSION);
     return (
         <div className='main'>
-            <span>00 Team solam</span>
+            <span>00 Team</span>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+    entry: './src/App.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        sourceMapFilename: 'SourceMaps/[file].map'
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: 'babel-loader'
                 }
             },
             {
                 test: [/\.s[ac]ss$/i, /\.css$/i],
-                use: ["style-loader", "css-loader", "sass-loader"]
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ]
     },
-    output: {
-      filename: "[name].js",
-      sourceMapFilename: "[name].js.map"
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '00 Team',
+            filename: 'index.html',
+            template: './src/template.html',
+            inject: true,
+            favicon: './src/img/favicon.ico',
+        }),
+    ],
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
     },
-    devtool: "source-map",
 };
