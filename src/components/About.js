ce3b73d2553da7ba2141cdb630d91a5b1bbfe76d
sadra <sadrataghavi1383@gmail.com>
2021-09-11 17:32:14 +0430

    worked on about us

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// import scss
+import './sass/about.scss'
+
+function About() {
+    return (
+        <div className='about'>
+            <div className='container'>
+                <div className='header'>
+                    <h1>what is 00 team</h1>
+                </div>
+                <div className='discription'>
+                    <p className='about-us-text'>
+                    00 Team is an Iranian, organized company that started its work in 2019. <br />
+                    We have several achievements on Fivem Scripts development and has worked mostly on Web Development.<br />
+                    Some of our works are for example writing HUDs, Score Board Menus, and so on.<br />
+                    our customers were many popular Fivem Iranian servers like: "Phonixe rp","moonlight rp", "justice city",...<br />
+                    Nevertheless, You Can Check Out Our Website, We Have All Of Our Projects Organized There For You. <br />
+                </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
