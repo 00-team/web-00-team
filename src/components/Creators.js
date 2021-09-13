bbc7254a7a34d08cc21d5c7695b8b49afe26a65a
sadra <sadrataghavi1383@gmail.com>
2021-09-13 15:10:07 +0430

    creators section is basically done

--- src/components/Creators.js
+++ src/components/Creators.js
@@ -1,19 +1,33 @@
 import React from 'react'
 
+// icons
+import { FiGithub } from 'react-icons/fi'
+import { SiDiscord } from 'react-icons/si'
+
 // import css
 import './sass/creators.scss'
 
 // imgs
-import aria from '../img/aria.jpg'
+import img_007 from '../img/007.png'
 import sadra from '../img/sadra.png'
 
 var creators_status = [
     {
-        img: aria,
+        img: sadra,
         title: 'Sadra',
-        discription: 'Perfect People Don`t Exist So Don`t Pretend To Be One',
+        background: 'grey',
+        role: '00 Team Developer',
+        discription: "perfect people don't exist, so don't pretend to be one",
+        github: 'https://github.com/SadraTghvi',
     },
-    { img: sadra, title: '007', discription: 'The Time Of Maddnes Has Come' },
+    {
+        img: img_007,
+        title: '007',
+        background: 'black',
+        role: '00 Team Developer',
+        discription: 'time of Madness has come',
+        github: 'https://github.com/i007c',
+    },
 ]
 
 function Creators() {
@@ -23,7 +37,33 @@ function Creators() {
                 <div className='header'>
                     <h1>Creators</h1>
                 </div>
-                
+                <div className='cards'>
+                    {creators_status.map((obj, index) => {
+                        return (
+                            <div className='card' key={index}>
+                                <div
+                                    className='creator-img'
+                                    style={{ backgroundColor: obj.background }}
+                                >
+                                    <img src={obj.img} />
+                                </div>
+                                <div className='text-container'>
+                                    <h1 className='title'>{obj.title}</h1>
+                                    <h4 className='role'>{obj.role}</h4>
+                                    <p className='discription'>
+                                        {obj.discription}
+                                    </p>
+                                    <div
+                                        className='social github'
+                                        onClick={e => window.open(obj.github)}
+                                    >
+                                        Github <FiGithub />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
