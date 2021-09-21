01babdfd10f70675f74f04e8182b15cb69e35a76
sadra <sadrataghavi1383@gmail.com>
2021-09-22 00:35:40 +0330

    added signup comp to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// router
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 // components
 import Head from './components/Head'
 import HeroSection from './components/HeroSection'
@@ -9,6 +12,9 @@ import Creators from './components/Creators'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import JoinUs from './components/JoinUs'
+
+// auth
+import Signup from './components/auth/Signup'
 
 // firebase
 import firebase from 'firebase/compat/app'
@@ -20,19 +26,25 @@ const App = () => {
     console.log(firebase.SDK_VERSION)
 
     return (
-        <>
-            {/* Head Tags */}
-            <Head />
-
+        <Router>
             <Navbar />
+            <Head />
+            <Switch>
+                <Route path='/' exact>
+                    {/* Head Tags */}
 
-            <HeroSection />
+                    <HeroSection />
 
-            <About />
-            <Creators />
-            <Projects />
-            <JoinUs />
-        </>
+                    <About />
+                    <Creators />
+                    <Projects />
+                    <JoinUs />
+                </Route>
+                <Route path='/signup'>
+                    <Signup />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
