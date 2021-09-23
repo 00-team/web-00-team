cf47fee77eb4a7e2d956c4e63e54f01cd1963b4a
i007c <dr007cc@gmail.com>
2021-09-24 00:46:48 +0330

    move navbar to layouts

---
+++ src/layouts/Background.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// style
+import './sass/background.scss'
+
+const Background = () => {
+    return (
+        <div className='background-container'>
+            
+        </div>
+    )
+}
+
+export default Background
