ad5146c395d4aebf147ebcec07127663ea72f23c
i007c <dr007cc@gmail.com>
2021-09-23 06:52:22 +0330

    useing markdown for about from graphcms

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// markdowm
+import Markdown from 'markdown-to-jsx';
+// reudx
+import { useSelector, useDispatch } from 'react-redux'
+import loadBase from '../redux/actions/data/loadBase'
 
 // import scss
 import './sass/about.scss'
 
 function About() {
+    const dispatch = useDispatch()
+    const BaseState = useSelector(state => state.Base.base)
+
+    useEffect(() => {
+        dispatch(loadBase())
+    }, [dispatch])
+
+    if (!BaseState || !BaseState.about || !BaseState.about.markdown) return <></>
+
     return (
         <div className='about'>
             <div className='container'>
                 <div className='header'>
-                    <h1 id="00team">what is 00 team</h1>
+                    <h1 id='00team'>what is 00 team</h1>
                 </div>
                 <div className='discription'>
-                    <p className='about-us-text'>
-                    00 Team is an Iranian, organized company that started its work in 2019. <br />
-                    We have several achievements on Fivem Scripts development and has worked mostly on Web Development.<br />
-                    Some of our works are for example writing HUDs, Score Board Menus, and so on.<br />
-                    our customers were many popular Fivem Iranian servers like: "Phonixe rp","moonlight rp", "justice city",...<br />
-                    Nevertheless, You Can Check Out Our Website, We Have All Of Our Projects Organized There For You. <br />
-                </p>
+                    <div className='about-us-text'>
+                        <Markdown >{BaseState.about.markdown}</Markdown>
+                    </div>
                 </div>
             </div>
         </div>
