f743739d8b120d9e5a8b8e7ecd2b20b454d71234
i007c <dr007cc@gmail.com>
2021-09-25 00:49:40 +0330

    inline loading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // redux stuff
-import { Provider as ReduxProvider } from 'react-redux'
+import { Provider as ReduxProvider, useSelector } from 'react-redux'
 import store from './redux/store'
 
 // router
@@ -22,10 +22,65 @@ import Navbar from './layouts/Navbar'
 import './sass/base.scss'
 import './sass/fonts/imports.scss'
 
+const LoadingStatus = () => {
+    const state = useSelector(state => state)
+    if (typeof state === 'object') {
+        return Object.values(state)
+            .filter(i => i.loading !== undefined)
+            .map(i => i.loading)
+    } else {
+        return null
+    }
+}
+
 const App = () => {
+    let loadings = LoadingStatus()
+
     return (
         <>
             <Navbar />
+
+            {/* loading ... */}
+            {loadings.some(i => i) && (
+                <div
+                    style={{
+                        position: 'fixed',
+                        zIndex: 100,
+                        inset: '0 0 0 0',
+                        background: 'black',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        gap: '20px',
+                        color: '#fff',
+                    }}
+                >
+                    <span>Loading ...</span>
+                    <span
+                        style={{
+                            width: '40%',
+                            height: '10px',
+                            background: '#FFF1',
+                            overflow: 'hidden',
+                            borderRadius: 10,
+                        }}
+                    >
+                        <span
+                            style={{
+                                background: '#FFF9',
+                                width: `${Math.floor(
+                                    (loadings.filter(i => !i).length /
+                                        loadings.length) *
+                                        100
+                                )}%`,
+                                height: '100%',
+                                display: 'block',
+                            }}
+                        ></span>
+                    </span>
+                </div>
+            )}
 
             <Switch>
                 <Route path='/' exact>
