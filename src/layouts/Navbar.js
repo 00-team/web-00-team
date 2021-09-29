c78851788d981f7e3f1d61fdb8d3cbd3606f89db
sadra <sadrataghavi1383@gmail.com>
2021-09-29 20:06:18 +0330

    removed '- 100' from scroll values

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 
-// redux 
-import { useSelector } from 'react-redux';
+// redux
+import { useSelector } from 'react-redux'
 
 // link
 import { Link } from 'react-router-dom'
 
-// icons 
-import { GrProjects } from 'react-icons/gr';
+// icons
+import { GrProjects } from 'react-icons/gr'
 import { BsQuestionSquareFill } from 'react-icons/bs'
 import { GiTeamIdea, GiFilmProjector } from 'react-icons/gi'
 import { FaHandshake } from 'react-icons/fa'
@@ -16,32 +16,31 @@ import { FaHandshake } from 'react-icons/fa'
 import './sass/navbar.scss'
 
 const Navbar = () => {
-
     const elementsScrollTop = useSelector(state => state.scrollTop)
 
-    const scrollToProjects = () =>{
+    const scrollToProjects = () => {
         window.scrollTo({
-            top: elementsScrollTop.projects - 100,
+            top: elementsScrollTop.projects,
             behavior: 'smooth',
         })
     }
-    const scrollToCreators= () =>{
+    const scrollToCreators = () => {
         window.scrollTo({
-            top: elementsScrollTop.creators - 100,
+            top: elementsScrollTop.creators,
             behavior: 'smooth',
         })
     }
-    
+
     const scrollToJoin = () => {
         window.scrollTo({
-            top: elementsScrollTop.join - 100,
+            top: elementsScrollTop.join,
             behavior: 'smooth',
         })
     }
 
     const scrollToAbout = () => {
         window.scrollTo({
-            top: elementsScrollTop.about - 100,
+            top: elementsScrollTop.about,
             behavior: 'smooth',
         })
     }
