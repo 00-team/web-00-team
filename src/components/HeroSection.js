d595e54995d928f209b44f3be3e219d1bceb9c51
i007c <dr007cc@gmail.com>
2021-09-30 23:18:10 +0330

    clean code

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,29 +1,30 @@
 import React from 'react'
 
-// import scss 
-import "./sass/herosection.scss"
+// import scss
+import './sass/herosection.scss'
 
 // icons
 import { FiGithub } from 'react-icons/fi'
 import { SiDiscord } from 'react-icons/si'
 
-
 function HeroSection() {
     return (
-      <div className="hero-section">
-        <div className="main">
-          <span>00 Team</span>
-          <div className="social">
-            <SiDiscord
-              onClick={(e) => window.open("https://discord.gg/Z6vgXHU2xQ")}
-            />
-            <FiGithub
-              onClick={(e) => window.open("https://github.com/00-team")}
-            />
-          </div>
+        <div className='hero-section'>
+            <div className='main'>
+                <span>00 Team</span>
+                <div className='social'>
+                    <SiDiscord
+                        onClick={e =>
+                            window.open('https://discord.gg/Z6vgXHU2xQ')
+                        }
+                    />
+                    <FiGithub
+                        onClick={e => window.open('https://github.com/00-team')}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    );
+    )
 }
 
 export default HeroSection
