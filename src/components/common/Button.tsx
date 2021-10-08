a35c999935fe2443e1165bac2ac66eee513af449
i007c <dr007cc@gmail.com>
2021-10-08 09:02:58 +0330

    add onClick Prop for Button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,12 +5,19 @@ import './scss/button.scss'
 
 interface ButtonProps {
     children?: ReactNode
+    onClick?: (e: React.MouseEvent) => void
 }
 
-const defaultProps: ButtonProps = {}
+const defaultProps: ButtonProps = {
+    onClick: () => {},
+}
 
-const Button = ({ children }: ButtonProps) => {
-    return <button className='button'>{children}</button>
+const Button = ({ children, onClick }: ButtonProps) => {
+    return (
+        <button className='button' onClick={e => (onClick ? onClick(e) : {})}>
+            {children}
+        </button>
+    )
 }
 
 Button.defaultProps = defaultProps
