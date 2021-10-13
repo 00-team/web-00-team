251437f93e87d9dd92a93a42fb21b1c643b90226
sadra <sadrataghavi1383@gmail.com>
2021-10-13 11:26:31 +0330

    created elements and functionality for navbar (responsive mode)

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // link
 import { Link } from 'react-router-dom'
@@ -14,48 +14,81 @@ import { BsQuestionSquareFill } from '@react-icons/all-files/bs/BsQuestionSquare
 
 import { FaHandshake } from '@react-icons/all-files/fa/FaHandshake'
 
+//mobile icons
+import { FaBars } from '@react-icons/all-files/fa/FaBars'
+import { FaTimes } from '@react-icons/all-files/fa/FaTimes'
+
 // style
 import './sass/navbar.scss'
 
 const Navbar = () => {
+
+    const [click, setClick] = useState<boolean>(false)
+
+    //mobile functions
+    const toggleNavbar = () => setClick(!click)
+    const closeMoblieMenu = () => setClick(false)
+
+    //
+
     return (
         <div className='navbar-container'>
             <nav className='navbar'>
                 {/* <div className='logo'>00</div>*/}
 
-                <div className='section'>
-                    <Link className='link' to='/'>
+                <div className='menu-icon' onClick={toggleNavbar}>
+                    {click ? <FaTimes /> : <FaBars />}
+                </div>
+
+                <div className={`section ${click ? 'active' : ''}`}>
+                    <Link className='link' to='/' onClick={closeMoblieMenu}>
                         Home
                         <div className='navbar-icon'>
                             <IoHome size={22} />
                         </div>
                     </Link>
 
-                    <Link className='link' to='/about'>
+                    <Link
+                        className='link'
+                        to='/about'
+                        onClick={closeMoblieMenu}
+                    >
                         What is 00 Team
                         <div className='navbar-icon'>
                             <BsQuestionSquareFill size={22} />
                         </div>
                     </Link>
 
-                    <Link className='link' to='/projects'>
+                    <Link
+                        className='link'
+                        to='/projects'
+                        onClick={closeMoblieMenu}
+                    >
                         Projects
                         <div className='navbar-icon'>
                             <GiFilmProjector size={28} />
                         </div>
                     </Link>
-                    <Link className='link' to='/creators'>
+                    <Link
+                        className='link'
+                        to='/creators'
+                        onClick={closeMoblieMenu}
+                    >
                         Creators
                         <div className='navbar-icon'>
                             <GiTeamIdea size={28} />
                         </div>
                     </Link>
-                    <a className='link'>
+                    <Link
+                        className='link'
+                        to='/wanajoin'
+                        onClick={closeMoblieMenu}
+                    >
                         Wana Join?
                         <div className='navbar-icon'>
                             <FaHandshake size={28} />
                         </div>
-                    </a>
+                    </Link>
                 </div>
             </nav>
         </div>
