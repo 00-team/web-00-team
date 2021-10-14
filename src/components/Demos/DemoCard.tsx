0a5797b38b92f09534fd232c3f292b602915ccab
i007c <dr007cc@gmail.com>
2021-10-14 20:27:35 +0330

    split ProjectinMain to Demos

---
+++ src/components/Demos/DemoCard.tsx
@@ -0,0 +1,102 @@
+import React, { FC } from 'react'
+
+// packages
+import Markdown from 'markdown-to-jsx'
+
+// utils
+import { go } from '../utils'
+
+// redux
+import { ProjectModel } from 'src/redux/models/Projects'
+
+// icons
+import { FiGithub } from '@react-icons/all-files/fi/FiGithub'
+import { ImArrowUp2 } from '@react-icons/all-files/im/ImArrowUp2'
+
+// commons
+import Button, { ButtonWithArrow } from '../common/Button'
+import Toucher from '../common/Toucher'
+
+import './demo-card.scss'
+
+interface DemoCardProps {
+    demo: ProjectModel
+    index: number
+    showDetail: string
+    setshowDetail: React.Dispatch<React.SetStateAction<string>>
+}
+
+const DemoCard: FC<DemoCardProps> = ({
+    demo,
+    index,
+    setshowDetail,
+    showDetail,
+}) => {
+    const onToucher = (m: number) => {
+        if (m > 70) {
+            setshowDetail(`${index}`)
+        } else if (m < -70) {
+            setshowDetail('')
+        }
+    }
+
+    return (
+        <Toucher
+            className='demo-card'
+            onToucher={m => onToucher(m)}
+            ToucherDir='Y'
+        >
+            <div
+                style={
+                    demo.thumbnail
+                        ? {
+                              backgroundImage: `url(${demo.thumbnail.url})`,
+                          }
+                        : {}
+                }
+                className='thumbnail'
+            ></div>
+            <div
+                onMouseUp={() => setshowDetail(showDetail ? '' : `${index}`)}
+                className={`details ${
+                    showDetail === `${index}` ? 'focus' : ''
+                }`}
+            >
+                <div
+                    className={`arrow-up ${
+                        showDetail === `${index}` ? 'active' : ''
+                    }`}
+                >
+                    <ImArrowUp2 size={24} className='arrow-up-icon' />
+                </div>
+                <h2 className='title'>{demo.title}</h2>
+
+                {/* description  */}
+                {(demo.description && (
+                    <Markdown className='description'>
+                        {demo.description.markdown}
+                    </Markdown>
+                )) || <div className='description'>No Desc</div>}
+                <div className='see-more'>
+                    {demo.git && (
+                        <Button
+                            classname='giticon'
+                            onClick={() => go(demo.git)}
+                        >
+                            GitHub <FiGithub />
+                        </Button>
+                    )}
+
+                    <ButtonWithArrow
+                        classname='project'
+                        onClick={() => go(`/project/${demo.projectSlug}`)}
+                    >
+                        See More
+                    </ButtonWithArrow>
+                </div>
+            </div>
+        </Toucher>
+    )
+}
+
+export default DemoCard
--- src/components/Demos/Demos.tsx
+++ src/components/Demos/Demos.tsx
@@ -1,14 +1,76 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+
+import Loadable from '@loadable/component'
+
+// redux stuff
+import { useDispatch, useSelector } from 'react-redux'
+import loadProjects from '../../redux/actions/loadProjects'
+import { RootState } from '../../redux'
+// import { ProjectModel } from 'src/redux/models/Projects'
+
+// commons
+import Loading from '../common/Loading'
+
+const LazyMotion = Loadable(() => import('../common/LazyMotion'))
+const CardSlider = Loadable(() => import('../common/slider/CardSlider'))
+
+// import style
+import './demos.scss'
+import DemoCard from './DemoCard'
 
 interface DemosProps {
     LoadingRender?: boolean
 }
 
-const defaultProps: DemosProps = {}
+const defaultProps: DemosProps = {
+    LoadingRender: true,
+}
 
-const Demos: FC<DemosProps> = ({ LoadingRender }) => {
-    console.log(LoadingRender)
-    return <div></div>
+export const Demos: FC<DemosProps> = ({ LoadingRender }) => {
+    const dispatch = useDispatch()
+    const Projects = useSelector((state: RootState) => state.Projects)
+    const DemosData = useSelector((state: RootState) => state.Projects.projects)
+
+    const [showDetail, setshowDetail] = useState('')
+
+    useEffect(() => {
+        dispatch(loadProjects())
+    }, [dispatch])
+
+    if (Projects.error) {
+        return <span>Error Acquired {Projects.error}</span>
+    }
+
+    if (Projects.loading && LoadingRender) {
+        return <Loading />
+    }
+
+    if (DemosData.length === 0) return <></>
+
+    return (
+        <div className='demos'>
+            <div className='container'>
+                <LazyMotion>
+                    <div className='header'>
+                        <h1>Demos</h1>
+                    </div>
+                </LazyMotion>
+                <div className='demo-slider'>
+                    <CardSlider onChange={() => setshowDetail('')}>
+                        {DemosData.map((item, index) => (
+                            <DemoCard
+                                key={index}
+                                demo={item}
+                                showDetail={showDetail}
+                                setshowDetail={setshowDetail}
+                                index={index}
+                            />
+                        ))}
+                    </CardSlider>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 Demos.defaultProps = defaultProps
