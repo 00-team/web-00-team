4f5de814ee844b23838a4d683222f757abce8f67
i007c <dr007cc@gmail.com>
2021-10-17 14:46:23 +0330

    make component for team

---
+++ src/components/Team.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+
+import './sass/team.scss'
+
+const Team: FC = () => {
+    return (
+        <div className='team'>
+            <h1 className='title'>00 Team</h1>
+            <span className='text'>believe your dreams</span>
+        </div>
+    )
+}
+
+export default Team
