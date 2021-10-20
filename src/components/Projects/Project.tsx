bfc3231dae5525546332cf27128c5fa63d9b282f
sadra <sadrataghavi1383@gmail.com>
2021-10-21 00:01:34 +0330

    worked on project

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -11,6 +11,13 @@ import { RootState } from '../../redux'
 // elements
 import Loading from '../common/Loading'
 
+// icons 
+import { FiGithub } from '@react-icons/all-files/fi/FiGithub'
+import { AiOutlineClockCircle } from '@react-icons/all-files/ai/AiOutlineClockCircle'
+
+// styling 
+import "./sass/project.scss"
+
 interface ProjectParams {
     slug: string
 }
@@ -38,39 +45,68 @@ const Project = () => {
     }
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                color: '#FFF',
-                padding: '50px',
-            }}
-        >
-            Slug: {slug}
-            <br />
-            title: {Project.title}
-            <br />
-            thumbnail: {Project.thumbnail ? Project.thumbnail.url : 'None'}
-            <br />
-            description:{' '}
-            {Project.description ? Project.description.markdown : 'None'}
-            <br />
-            startDate: {Project.startDate}
-            <br />
-            Slug: {Project.projectSlug}
-            <br />
-            projectUrl: {Project.projectUrl}
-            <br />
-            git: {Project.git}
-            <br />
-            demos:
-            {Project.demos.map((i, ndx) => (
-                <div key={ndx}>
-                    <span>{i.url}</span>
-                    <br />
+        // <div className='project'>
+        //     <h1 className='title'> {Project.title} </h1>
+        //     <br />
+        //     <div className='prev'>
+        //         {Project.thumbnail ? Project.thumbnail.url : 'None'}
+        //     </div>
+
+        //     <br />
+        //     description:{' '}
+        //     {Project.description ? Project.description.markdown : 'None'}
+        //     <br />
+        //     startDate: {Project.startDate}
+        //     <br />
+        //     projectUrl: {Project.projectUrl}
+        //     <br />
+        //     git: {Project.git}
+        //     <br />
+        //     demos:
+        //     {Project.demos.map((i, ndx) => (
+        //         <div key={ndx}>
+        //             <span>{i.url}</span>
+        //             <br />
+        //         </div>
+        //     ))}
+        //     <br />
+        // </div>
+        <div className='project'>
+            <div className='card'>
+                <div className='side left'>
+                    <div
+                        className='img'
+                        style={{
+                            backgroundImage: `url(${
+                                Project.thumbnail
+                                    ? Project.thumbnail.url
+                                    : 'None'
+                            })`,
+                        }}
+                    ></div>
                 </div>
-            ))}
-            <br />
+                <div className='side right'>
+                    <h2 className='title'>{Project.title}</h2>
+                    <div className='description'>
+                        {Project.description
+                            ? Project.description.markdown
+                            : 'None'}
+                    </div>
+                    <div className='data'>
+                        <div className='start-date'>
+                            <AiOutlineClockCircle />
+                            <div className='start-date-url'>
+                                {Project.startDate}
+                            </div>
+                        </div>
+                        <div className='github'>
+                            <FiGithub />
+                            <div className='github-url'>{Project.git}</div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className='notify'></div> */}
+            </div>
         </div>
     )
 }
