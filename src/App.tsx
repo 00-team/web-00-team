dc031e7f5b0d0b6aac36dfaf00870ab3a8e16804
i007c <dr007cc@gmail.com>
2021-10-23 23:59:34 +0330

    removing react-snap

--- src/App.tsx
+++ src/App.tsx
@@ -147,14 +147,6 @@ const Root: FC = () => {
     )
 }
 
-const RootElement = document.getElementById('root')
-
-if (RootElement) {
-    if (RootElement.hasChildNodes()) {
-        ReactDOM.hydrate(<Root />, RootElement)
-    } else {
-        ReactDOM.render(<Root />, RootElement)
-    }
-}
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 export default App
